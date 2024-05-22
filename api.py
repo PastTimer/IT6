343468b6abed64c44dd2186603a731daf5ce9b6e
Imee Vito <vitoimee@gmail.com>
2024-05-22 21:50:59 +0800

    Added routes on web app, total of five required tables

--- api.py
+++ api.py
@@ -27,6 +27,54 @@ def get_events():
     
     return make_response(jsonify(data), 200)
 
+@app.route("/members", methods=["GET"])
+def get_members():
+    cur = mysql.connection.cursor()
+    query="""
+    select * from members;
+    """
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    
+    return make_response(jsonify(data), 200)
+
+@app.route("/hobbies_and_pasttime", methods=["GET"])
+def get_hobbies_and_pasttime():
+    cur = mysql.connection.cursor()
+    query="""
+    select * from hobbies_and_pasttime;
+    """
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    
+    return make_response(jsonify(data), 200)
+
+@app.route("/organizations", methods=["GET"])
+def get_organizations():
+    cur = mysql.connection.cursor()
+    query="""
+    select * from organizations;
+    """
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    
+    return make_response(jsonify(data), 200)
+
+@app.route("/memberships", methods=["GET"])
+def get_memberships():
+    cur = mysql.connection.cursor()
+    query="""
+    select * from memberships;
+    """
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    
+    return make_response(jsonify(data), 200)
+
 if __name__ == "__main__":
     app.run(debug=True)
     
